171a11c7228f4e715189a6c8b2b3dbbcfefc2a78
Justin Huy <trannhathuy1305@gmail.com>
2019-12-02 20:08:44 +0700

    Add some scroll effect on buttons and navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,41 @@
 $(document).ready(function () {
-
-    $('.js--section-feature').waypoint(function(direction) {
-        if(direction == 'down') {
+    /* For the sticky navigation */
+    $('.js--section-feature').waypoint(function (direction) {
+        if (direction == 'down') {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
     }, {
         offset: '60px'
-      });
+    });
 
-    /* var waypoints = $('#handler-first').waypoint(function(direction) {
-        notify(this.element.id + ' hit 25% from top of window') 
-      }, {
-        offset: '25%'
-      }) */
+    /* Scroll on buttons*/
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
 
-})
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-feature').offset().top
+        }, 1000);
+    });
+
+    /* Navigation scroll */
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+});
